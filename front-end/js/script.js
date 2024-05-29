371e593f33e6c7a20d9968e72aab0c0ea5911848
Rafael Batista <rafael.aba@outlook.com>
2024-05-29 03:01:19 -0300

    better front

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,3 +1,40 @@
-function FindLaptops() {
-    alert('finding laptop')
+const mock_list = [
+    {
+        "name": "tarefa 0",
+        "status": "ready"
+    },
+    {
+        "name": "tarefa 1",
+        "status": "ready"
+    },
+    {
+        "name": "tarefa 2",
+        "status": "progress"
+    },
+    {
+        "name": "tarefa 3",
+        "status": "done"
+    }
+]
+
+function FillList() {
+    let list = document.getElementById("list");
+
+    mock_list.forEach((item) => {
+        let div = document.createElement("div");
+        div.className = "to-do-element"
+        let title = document.createElement("p");
+        title.innerHTML = item.name
+        title.className = "to-do-title"
+        let status = document.createElement("p");
+        status.innerHTML = item.status
+        status.className = "to-do-status"
+
+        div.appendChild(title)
+        div.appendChild(status)
+        list.appendChild(div);
+    });
 }
+
+FillList()
+
